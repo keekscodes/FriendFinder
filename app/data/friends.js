5856aba0de88cb3f2e75516e47b810aef334cadd
Kimberly Mueller <kamueller2@gmail.com>
2019-07-05 18:08:14 -0400

    added male friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,7 +12,16 @@ var friends = [{
     {
         name: 'Madison',
         photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/02-Madison-Beer-press-cr-Hudson-Taylor-billboard-1548.jpg',
-        scores: `[5, 3, 4, 2, 5, 5, 3, 5, 1, 1]`
+        scores: `[5, 6, 4, 2, 5, 5, 3, 5, 1, 1]`
+    },
+    {
+        name: 'Drake',
+        photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NTI2OTA4NzY5MjE2MTI4/drake_photo_by_prince_williams_wireimage_getty_479503454.jpg',
+        scores: `[4, 2, 3, 5, 5, 1, 2, 5, 4, 6]`
+    },
+    {
+        name: 'Chris',
+        photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU1MTM0NzA2NzQ3MTg4NDQw/chris-evans-arrives-at-the-los-angeles-premiere-of-captain-america-the-winter-soldier-held-at-the-el-capitan-theatre-on-march-13-2014-in-hollywood-california-photo-by-michael-tran_filmmagicjpg-square.jpg',
+        scores: `[2, 6, 3, 2, 4, 5, 3, 4, 5, 1]`
     }
-
 ]
