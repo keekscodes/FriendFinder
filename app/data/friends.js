529aad2878287e17e3cff178093fc893ca7cadee
Kimberly Mueller <kamueller2@gmail.com>
2019-07-05 18:02:04 -0400

    created inital friends

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+// Object holding friends
+var friends = [{
+        name: 'Britney',
+        photo: 'https://live.staticflickr.com/5216/5387285442_5584d1600d_z.jpg',
+        scores: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+    },
+    {
+        name: 'Kourtney',
+        photo: 'https://www.byrdie.com/thmb/i0h8kcobx6gVmvkIiU7Q6UlWCgI=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__cache__posts__208515__-1978640-1479263790.700x0c-cb05806356db445a82c4904a424bed47.jpg',
+        scores: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+    },
+    {
+        name: 'Madison',
+        photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/02-Madison-Beer-press-cr-Hudson-Taylor-billboard-1548.jpg',
+        scores: `[5, 3, 4, 2, 5, 5, 3, 5, 1, 1]`
+    }
+
+]
