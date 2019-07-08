8c68afc284585ac88f8ae3f207c95769e9486c38
Kimberly Mueller <kamueller2@gmail.com>
2019-07-08 17:22:54 -0400

    fixed file path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@ var path = require('path');
 module.exports = function(app) {
     // A GET route to display all possible friends
     app.get('/survey', function(request, response) {
-        response.sendFile(path.join(__dirname + '../public/survey.html'));
+        response.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
 
     // A default USE route that leads to home.html
     app.use(function(request, response) {
-        response.sendFile(path.join(__dirname + '../public/home.html'));
+        response.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 
 };
