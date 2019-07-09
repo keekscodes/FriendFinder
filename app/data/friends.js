1bbae4729f2cd42cee2b3958d042069e13674cf2
Kimberly Mueller <kamueller2@gmail.com>
2019-07-09 16:26:48 -0400

    fixed friends array syntax

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@
 let friends = [{
         name: 'Britney',
         photo: 'https://live.staticflickr.com/5216/5387285442_5584d1600d_z.jpg',
-        scores: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         name: 'Kourtney',
         photo: 'https://www.byrdie.com/thmb/i0h8kcobx6gVmvkIiU7Q6UlWCgI=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__cache__posts__208515__-1978640-1479263790.700x0c-cb05806356db445a82c4904a424bed47.jpg',
-        scores: `[3, 2, 5, 4, 5, 1, 2, 5, 4, 1]`
+        scores: [3, 2, 5, 4, 5, 1, 2, 5, 4, 1]
     },
     {
         name: 'Madison',
         photo: 'https://www.billboard.com/files/styles/article_main_image/public/media/02-Madison-Beer-press-cr-Hudson-Taylor-billboard-1548.jpg',
-        scores: `[5, 5, 4, 2, 5, 5, 3, 5, 1, 1]`
+        scores: [5, 5, 4, 2, 5, 5, 3, 5, 1, 1]
     },
     {
         name: 'Drake',
         photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQ3NTI2OTA4NzY5MjE2MTI4/drake_photo_by_prince_williams_wireimage_getty_479503454.jpg',
-        scores: `[4, 2, 3, 5, 5, 1, 2, 5, 4, 5]`
+        scores: [4, 2, 3, 5, 5, 1, 2, 5, 4, 5]
     },
     {
         name: 'Chris',
         photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU1MTM0NzA2NzQ3MTg4NDQw/chris-evans-arrives-at-the-los-angeles-premiere-of-captain-america-the-winter-soldier-held-at-the-el-capitan-theatre-on-march-13-2014-in-hollywood-california-photo-by-michael-tran_filmmagicjpg-square.jpg',
-        scores: `[4, 4, 5, 4, 5, 2, 3, 5, 4, 5]`
+        scores: [4, 4, 5, 4, 5, 2, 3, 5, 4, 5]
     },
     {
         name: 'Michael',
